d2cbe1807ea7bda59459d51c00c1414c5cf5216e
thakurprabhat <90917125+thakurprabhat@users.noreply.github.com>
2023-01-08 12:07:50 +0530

    WP: Add enviroment file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
-const port = 3000;
+const port = process.env.port || 3000;
 
 
 // pass path of html content
